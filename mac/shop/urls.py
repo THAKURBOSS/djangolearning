561c667b2a1d5a0b5a76380a36c571966b9ec2d9
lucky <avichalsingh1935@gmail.com>
2022-08-29 20:04:14 +0530

    updated till video 25

--- mac/shop/urls.py
+++ mac/shop/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index,name="ShopHome")
+    path('',views.index,name="ShopHome"),
+    path('about/',views.about,name="AboutUs"),
+    path('contactus/',views.contactus,name="ContactUs"),
+    path('tracker/',views.tracker,name="Tracker"),
+    path('search/',views.search,name="Search"),
+    path('productview/',views.productview,name="ProductView"),
+    path('checkout/',views.checkout,name="Checkout")
+    
 ]
--- mac/shop/views.py
+++ mac/shop/views.py
@@ -5,3 +5,22 @@ from django.http import HttpResponse
 # Crete your views here.
 def index(request):
     return render(request,'shop/index.html')
+
+
+def about(request):
+    return HttpResponse('we are at about') 
+
+def contactus(request):
+    return HttpResponse('we are at contactus')
+
+def tracker(request):
+    return HttpResponse('we are at tracker')
+
+def search(request):
+    return HttpResponse('we are at search')
+
+def productview(request):
+    return HttpResponse('we are at productview')
+
+def checkout(request):
+    return HttpResponse('we are at checkout')
